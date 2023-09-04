de11810d1db0271b36715f818388fe9512a6416f
gangto33 <142385671+gangto33@users.noreply.github.com>
2023-09-04 18:37:28 +0900

    Add files via upload

---
+++ js/chat.js
@@ -0,0 +1,40 @@
+const $input = document.querySelector('input')
+const $button = document.querySelector('button')
+const $answer = document.querySelector('.answer')
+
+const data = []
+data.push({
+    "role": "system",
+    "content": "assistant는 감정과 상황에 맞는 클래식 음악을 5개 추천해주는 전문가이다."
+})
+
+const url = `https://estsoft-openai-api.jejucodingcamp.workers.dev/`
+
+$button.addEventListener('click', e => {
+    e.preventDefault()
+    const contents = $input.value + " 클래식 곡은 영어로 알려주세요" + " 이전 질문은 잊어주세요." + " 곡은 꼭 5개 알려주세요."
+    data.push({
+        "role": "user",
+        "content": contents
+    })
+    $input.value = ''
+
+    chatGPTAPI()
+})
+
+function chatGPTAPI() {
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data),
+        redirect: 'follow'
+    })
+    .then(res => res.json())
+    .then(res => {
+        console.log(res)
+        // 답변 온 것을 assistant로 저장
+        $answer.innerHTML = `<p>${res.choices[0].message.content}</p>`
+    })
+}
