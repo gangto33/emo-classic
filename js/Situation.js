08a79afdf363acc7342f4c17e776399954e0b590
gangto33 <142385671+gangto33@users.noreply.github.com>
2023-09-06 16:29:32 +0900

    Update Situation.js

--- js/Situation.js
+++ js/Situation.js
@@ -1,6 +1,7 @@
 const $input = document.querySelector('input')
-const $button = document.querySelector('button')
+const $button = document.querySelector('.emoButton')
 const $answer = document.querySelector('.answer')
+const $chachButton = document.querySelector('.chachButton')
 
 const $link1 = document.querySelector('.link1')
 const $link2 = document.querySelector('.link2')
@@ -19,7 +20,7 @@ const url = `https://estsoft-openai-api.jejucodingcamp.workers.dev/`
 
 $button.addEventListener('click', e => {
     e.preventDefault()
-    const contents = $input.value + " 곡은 꼭 1 ~ 5 를 사용한 형식으로 5개 알려주세요." + " 클래식 곡명은 꼭 영어만 사용하세요"
+    const contents = $input.value + " 5개의 곡을 1. ~ 5. 를 사용해 알려주세요." + " 클래식 곡명과 작곡가는 꼭 영어로 알려주세요" + " 장소에 대한 얘기도 해주세요."
     data.push({
         "role": "user",
         "content": contents
@@ -27,6 +28,9 @@ $button.addEventListener('click', e => {
     $input.value = ''
 
     chatGPTAPI()
+
+    // 버튼 생성
+    $chachButton.classList.add("getButton")
 
     // 데이터 초기화
     data.pop();
@@ -59,7 +63,7 @@ function chatGPTAPI() {
         let lastLink = link.split(/([가-힣])/)
 
         //  곡명 앞에 한글이 붙을 경우의 오류 방지
-        if (lastLink[0] === ' ') {
+        if (lastLink[0].length<5) {
             link = lastLink.pop().split(/([가-힣])/)
             linkList[4] = link[0]
         } else {
@@ -67,13 +71,42 @@ function chatGPTAPI() {
         }
 
         // 링크 정제 과정
-
+        for(var i=0; i<5; i++){
+            linkList[i] = linkList[i].replace(/,/g, '').replace(/[가-힣]/g, '')
+        }
         
         // 유튜브 링크로 만들어 보내기
-        $link1.innerHTML = `<a href="https://www.youtube.com/results?search_query= ${encodeURIComponent(linkList[0])}" target="_blank">${linkList[0]}</a>`
-        $link2.innerHTML = `<a href="https://www.youtube.com/results?search_query= ${encodeURIComponent(linkList[1])}" target="_blank">${linkList[1]}</a>`
-        $link3.innerHTML = `<a href="https://www.youtube.com/results?search_query= ${encodeURIComponent(linkList[2])}" target="_blank">${linkList[2]}</a>`
-        $link4.innerHTML = `<a href="https://www.youtube.com/results?search_query= ${encodeURIComponent(linkList[3])}" target="_blank">${linkList[3]}</a>`
-        $link5.innerHTML = `<a href="https://www.youtube.com/results?search_query= ${encodeURIComponent(linkList[4])}" target="_blank">${linkList[4]}</a>`
+        $link1.innerHTML = `<input type="checkbox" value='${linkList[0]}'/><a href="https://www.youtube.com/results?search_query= ${encodeURIComponent(linkList[0])}" target="_blank">${linkList[0]}</a>`
+        $link2.innerHTML = `<input type="checkbox" value='${linkList[1]}'/><a href="https://www.youtube.com/results?search_query= ${encodeURIComponent(linkList[1])}" target="_blank">${linkList[1]}</a>`
+        $link3.innerHTML = `<input type="checkbox" value='${linkList[2]}'/><a href="https://www.youtube.com/results?search_query= ${encodeURIComponent(linkList[2])}" target="_blank">${linkList[2]}</a>`
+        $link4.innerHTML = `<input type="checkbox" value='${linkList[3]}'/><a href="https://www.youtube.com/results?search_query= ${encodeURIComponent(linkList[3])}" target="_blank">${linkList[3]}</a>`
+        $link5.innerHTML = `<input type="checkbox" value='${linkList[4]}'/><a href="https://www.youtube.com/results?search_query= ${encodeURIComponent(linkList[4])}" target="_blank">${linkList[4]}</a>`
     })
+}
+
+// 캐시 데이터를 localStorage에 저장
+var j = 20;
+
+function getSelectedMusic() {
+
+    // localStorage에 데이터 저장
+    if (localStorage.getItem('j')){
+        j = parseInt(localStorage.getItem('j'))
+    }
+
+    let checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    let IsCheck = []
+    checkboxes.forEach(function(item) {
+        if (item.checked) {
+            IsCheck.push(item.value)
+        }
+    })
+
+    localStorage.setItem(j, IsCheck);
+    j++
+    localStorage.setItem('j', j);
+
+    checkboxes.forEach((checkbox) => {
+        checkbox.checked = false
+    });
 }
